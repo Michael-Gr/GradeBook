d4e4f2cc90f22ad6b6752c9f54cd1cbf6aa60bf7
Michael Grein <michaelgrein92@gmail.com>
2017-11-18 17:34:16 -0500

    Added class to compute various statistics about grades

--- GradeBookExample/GradeBook.cs
+++ GradeBookExample/GradeBook.cs
@@ -13,11 +13,30 @@ namespace GradeBookExample
             grades = new List<float>();
         }
 
-        List<float> grades;
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0;
+
+            foreach (float grade in grades)
+            {
+                sum += grade;
+
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+            }
+
+            stats.AverageGrade = sum / grades.Count;
+
+            return stats;
+        }
 
         public void AddGrade(float grade)
         {
             grades.Add(grade);
         }
+
+        List<float> grades;
     }
 }
